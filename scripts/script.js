76aca3960a88a6142dab6c30974d3b5f7aa86dec
EnzovanLeeuwen <70649858+EnzovanLeeuwen@users.noreply.github.com>
2020-10-21 17:25:33 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 // JavaScript Document
+document.getElementById("menuknop").addEventListener("click", menuKlik);
+
+function menuKlik() {
+    var dropdown = document.getElementById("dropdownmenu");
+    dropdown.classList.toggle("menuitems");
+
+}
